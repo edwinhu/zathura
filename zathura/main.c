6ecdb36fc919b7e7cabe5f2b71bfff5116b90466
Edwin Hu <eddyhu@gmail.com>
2026-01-08 10:07:22 -0500

    Add macOS file open handler for double-click support

--- zathura/main.c
+++ zathura/main.c
@@ -24,6 +24,30 @@
 #include "synctex.h"
 #endif
 
+#ifdef __APPLE__
+static zathura_t* g_macos_zathura = NULL;
+
+static gboolean macos_open_file_cb(GtkosxApplication* app, gchar* path, gpointer user_data) {
+  (void)app;
+  (void)user_data;
+
+  if (g_macos_zathura == NULL) {
+    girara_error("Cannot open file: zathura not initialized");
+    return FALSE;
+  }
+
+  if (path == NULL || strlen(path) == 0) {
+    girara_debug("Received empty path in open file callback");
+    return FALSE;
+  }
+
+  girara_debug("macOS open file request: %s", path);
+  document_open_idle(g_macos_zathura, path, NULL, ZATHURA_PAGE_NUMBER_UNSPECIFIED, NULL, NULL, NULL, NULL);
+
+  return TRUE;
+}
+#endif
+
 /* Handle synctex forward synchronization */
 #ifdef WITH_SYNCTEX
 static int run_synctex_forward(const char* synctex_fwd, const char* filename, int synctex_pid) {
@@ -285,6 +309,8 @@ GIRARA_VISIBLE int main(int argc, char* argv[]) {
 
 #ifdef __APPLE__
   GtkosxApplication* zathuraApp = g_object_new(GTKOSX_TYPE_APPLICATION, NULL);
+  g_macos_zathura = zathura;
+  g_signal_connect(zathuraApp, "NSApplicationOpenFile", G_CALLBACK(macos_open_file_cb), NULL);
   gtkosx_application_set_use_quartz_accelerators(zathuraApp, FALSE);
   gtkosx_application_ready(zathuraApp);
   {
